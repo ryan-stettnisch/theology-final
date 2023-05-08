d68471926219d8529b28d9e657eaaa62cc792880
ryan-stettnisch <ryanstetty@gmail.com>
2023-05-08 07:09:20 -0400

    Added all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Villanova_Wildcats_logo.svg/1200px-Villanova_Wildcats_logo.svg.png.jpg" alt="image-description" width="200" height="200" />
+          <br></br>
+          Religion and Ethics
+        
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <div className='App-body'>
+        <p>
+          Other Resources:
+        </p>
+      <a href="https://en.wikipedia.org/wiki/Ethics_in_religion">Ethics in religion</a>
+            <p></p>
+            <p>
+          This page leads to Wikipedia's summary of Ethics in Religion. It talks about how ethics plays a role in religions such as Buddhism, Christianity, Confucianism, and more.
+            </p>
+            <a href="https://archive.pagecentertraining.psu.edu/public-relations-ethics/ethical-decision-making/yet-another-test-page/the-difference-between-ethics-and-religion/#:~:text=While%20religion%20makes%20claims%20about,rather%20than%20tradition%20or%20injunction.">The Difference Between Ethics and Religion</a>
+            <p>
+              This page talks about the difference between Ethics and Religion, and explains further how religions try to create the best ways for their followers to live.
+            </p>
+              <p>
+              <a href="https://enlightio.com/religion-vs-ethics-what-is-the-difference">What's the Difference Between Religion and Ethics?</a>
+              <p>
+                This page leads to an article describing the differences between religion and ethics.
+              </p>
+              </p>
+              <p>
+              <a href="https://classroom.synonym.com/description-of-the-relationship-between-ethics-and-religious-beliefs-12086754.html">Description of the Relationship Between Ethics and Religious Beliefs</a>
+              </p>
+              <p>
+                This page also talks about the differences between religious belief and secular ethics.
+              </p>
+              <p>
+              <a href="https://www.scu.edu/mcae/publications/iie/v9n1/horizon.html#:~:text=Faith%2Dbased%20ethics%20considers%20the,new%20problems%20and%20new%20challenges.">The Horizon of Faith</a>
+              </p>
+              <p>
+                This page talks about how religion often provides "a point of reference for ethical decision making."
+              </p>
+              </div>
+      <div className='App-info'>
+      <body>
+            <p>
+            Ethics and religion have been connected throughout much of human history. Religion can help provide a solid framework of moral values for many people.
+            </p>
+      </body>
+      <div className='App-class'>
+        <body>
+          <p>
+            Resources from class:
+          </p>
+        </body>
+        <div className='App-class2'>
+          <body>
+            <p>Week 13: Living Ethically:</p>
+              
+             <p> <b>What's religious about ethics? (Or, what's ethical about religion?) </b>
+            </p>
+            <p>Rowan Williams - <a href="https://learn-us-east-1-prod-fleet02-xythos.content.blackboardcdn.com/5de826c99a8f3/14015326?X-Blackboard-S3-Bucket=learn-us-east-1-prod-fleet01-xythos&X-Blackboard-Expiration=1683536400000&X-Blackboard-Signature=nfV7f14LTD6YwPOxWr6YQL3iFIj9Yd8z8PLbSWL%2F7zc%3D&X-Blackboard-Client-Id=301650&X-Blackboard-S3-Region=us-east-1&response-cache-control=private%2C%20max-age%3D21600&response-content-disposition=inline%3B%20filename%2A%3DUTF-8%27%27making_moral_decisions%2520%25281%2529.pdf&response-content-type=application%2Fpdf&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEDwaCXVzLWVhc3QtMSJHMEUCIF8cQvchYbvWjcQt3aIosw0CuWMG3dVoc4LXlJPk10CKAiEA7SB%2BMubm9HQoBt5iAKlzd3oMtWFqmJGC1P6Pu0SgNn4qtAUIVRADGgw2MzU1Njc5MjQxODMiDFpdtrKVOxiVEgFgnyqRBRuCP9MKs9jyV9CMnjavO55WkiULgGoTBwdWAVVwUHoG%2BgWcVMSUiRfsRLYcELOQTeDDurlqcnRJzi4q0eD66AxtC2%2FGPjabM8HAUccqIQNvrCzGjP9eK%2BmSE9%2FcYt4cpX%2FCr%2BNS0yfPYRZu2dOD3tbqLMcVAt4Vehy2M24RpSOQSp6CTS%2FG5h5EMdRYljcjz6rWIPlMVW8DNRAkrMGfruemm5lhiJSLSNxPaEDiMkLtXCuQD65Zdo3sTkYik8JFF%2FjMybGk1b0DrkBuSprYaL6yxxFjWCk28mcP0Fe5xTOurKkAaMlHtPkOFhrAkj0%2FznBOjf6IzybAVxFs1l5lZSgEwau7g2GBbyKLvBtxGEm4xlu0JpRqL12Vp7LVgv2fgbFi%2BP9ADbMvL%2BBnhhVEsvZeUwa5tW53BqIU4tPcpNKwpoTL%2BQ2DucQT9KcLG%2B0VCmhn6u1a6PQakXWiCYT2vHo0Icl%2B75GaDteeFkNTGZ2G6swXsnybiIRN49nGdGadIlmltiARvaP5Uij2%2B51SFz%2BxVvsiQFxxuDAlAiLMrzHvrt%2FxYJwn5l7xpYCNZL6vFV%2FSK39U5vATHlCLw8yx11j8K4DWbm8a1oRzzj5YrT5RbFHUfTuj8v%2B9rNG%2BwhKKXqgcu4NKaX0SopjaKiFKHJBMCIppAK7vo70O1qCxIWCHrNsr%2FX37IcKCBIsnDm0KM35o0kwYVsvljkr5LcbPFYWKSbpbvPSC2hLOsqEZs4QvwUGdLcoV8K5Fstowy2ogatEPKJyHSiB4MPqOKL3WxylUEsfEjEXR83SF4nu6nmze40wYIB0ngCT86oWzjS3g04J3Ntm73e9nK017dJfq38%2FQqAIWoUqwd4HTQOb%2BnPopGjD73uGiBjqxAX%2FoTZixv5AVKYY8AcGhF92fq7vJsrmJ1cmTiKIrajA5B37Qm4Mqvv%2B1NtVVS%2F20Kqrfk9kmSs2O4a99q61aeXK6Vydc2rkSyccqz4Q7JqN%2B8sEzWtFDYowgz0GtWTPNEWsXMunULEd6hW7oIRmkWdHYTA0hQg4ZRgPTEvRa3v3vCdExQtfuZBsLSgs3aSjl0VVcsempdblRDwBjjdKYqsTjCjYBVWiG7Gu1hvuPqoaOBg%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20230508T030000Z&X-Amz-SignedHeaders=host&X-Amz-Expires=21600&X-Amz-Credential=ASIAZH6WM4PLZO2IXDPO%2F20230508%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=fc09d18459275ea315390e4ffd2db10216608fbc2e988a6ff00f3b85a7878fc6">Making moral decisions</a></p>
+          </body>
+          <p> <b>
+          What does it mean to live an ethical life in the face of disaster?
+          </b></p>
+          <p>Shannon Walsh - <a href="https://vums-web.villanova.edu/Mediasite/Play/7c4da5dcad464624a937cbb85a81ed981d">Illusions of Control</a></p>
+          <div className='App-video'>
+            <p>
+          <iframe width="420" height="415" src="https://www.youtube.com/embed/w9QtjQ5Ow7Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="420" height="415" src="https://www.youtube.com/embed/JU1CKvdM0ps" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
+          <iframe width="420" height="415" src="https://www.youtube.com/embed/zP0gi76VJyY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
+          <iframe width="425" height="415" src="https://www.youtube.com/embed/zG3wV8TkUCg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
+
+           </p>
+          
+          </div>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
